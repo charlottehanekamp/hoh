87f49b3751dc208a7e71fa1f1dc4f9cf00128a87
charlottehanekamp <charlotte@021.152.028.145.hva.nl>
2016-03-24 12:44:25 +0100

    default text

---
+++ app/entities/default_text_entity.rb
@@ -0,0 +1,9 @@
+class DefaultTextEntity < Grape::Entity
+  expose :title
+  expose :content_one
+  expose :content_two
+  expose :content_three
+  expose :content_four
+  expose :page
+  expose :image
+end
--- public/app.js
+++ public/app.js
@@ -1,4 +1,18 @@
 angular.module('hoh', ['ngCookies'])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get("/v1/default_text.json?page=index")
+    .success(function(data, status) {
+        if(status == 200){
+            console.log("succes");
+        }else {
+            $scope.messages_error = 'Oops, we received your request, but there was an error processing it.';
+        }
+        $scope.test="successs";
+        $scope.formData = data;
+        console.log(data);
+    })
+    .error(function(data, status, headers, config) {
+        $scope.messages_error = 'There was a network error. Try again later.';
+    });
 }]);
