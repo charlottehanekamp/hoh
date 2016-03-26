9f2fa28c45b283268d4eced73c42404215458dad
charlottehanekamp <charlotte@MacBook-Pro-van-Hanneke-2.local>
2016-03-26 22:10:12 +0100

    change users

--- app/controllers/api/v1/users.rb
+++ app/controllers/api/v1/users.rb
@@ -27,11 +27,41 @@ module API
         before do
           authenticate!
         end
+
         desc 'Show specific user profile'
         get do
-          # user = User.find(params[:user_id])
           present current_user, with: UserEntity
         end
+
+        params do
+          use :auth_params
+        end
+
+        before do
+          authenticate!
+        end
+        desc 'Update user profile'
+        params do
+        optional :first_name, type: String
+        optional :last_name, type: String
+        optional :email, type: String
+        optional :birth_date, type: String
+        optional :weight, type: Integer
+        optional :height, type: Integer
+        optional :content, type: String
+        optional :image, type: Rack::Multipart::UploadedFile
+        optional :password, type: String
+        given :password do
+          requires :password_confirmation, type: String
+        end
+      end
+      put do
+        user_params = declared(params.except(:token), include_missing: false)
+        error!(current_user.errors,422) unless current_user.update(user_params)
+        present current_user, with: UserEntity
+      end
+
+
       end
     end
   end
--- public/app/components/user/userController.js
+++ public/app/components/user/userController.js
@@ -1,4 +1,24 @@
 angular.module('hoh', ['ngCookies'])
-.controller('UserCtrl', ['$scope', '$http', '$location', '$cookies', '$cookieStore', function($scope, $http, $location, $cookies, $cookieStore){
-  $scope.username = $cookies.getObject('user').user.first_name;
+.controller('UserCtrl', ['$scope', '$cookies', '$cookieStore', '$http', function($scope, $cookies, $cookieStore, $http) {
+  $scope.formData = {};
+  $scope.formData.user = $cookies.getObject('user').user;
+  $scope.username = $scope.formData.user.first_name;
+  // console.log($scope.formData.user);
+
+  $scope.submitUser = function(){
+    $http.put("/v1/users.json?token=" + $cookies.getObject('user').access_token, {first_name: 'charlie'})
+      .success(function(data, status) {
+        console.log($scope.email);
+
+        $scope.new_profile = {access_token: $cookies.getObject('user').access_token, user: data};
+        console.log($scope.new_profile);
+      })
+      .error(function(data, status, headers, config) {
+        console.log("error");
+      })
+      .then(function(data) {
+        console.log("then");
+      });
+    console.log("Submit");
+  }
 }]);
