f85eae740a91ae657e96f9e999c8944e571e060c
charlie5991 <charlotte@hanekamp.be>
2016-04-18 22:36:39 +0200

    trying not working

--- public/app.js
+++ public/app.js
@@ -1,18 +1,22 @@
-angular.module('hoh', ['ngCookies'])
+var routerApp = angular.module('routerApp', ['ui.router']);
 
-.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
-  $http.get("/v1/default_text.json?page=index")
-    .success(function(data, status) {
-        if(status == 200){
-            console.log("succes");
-            $scope.test="successs";
-            $scope.formData = data;
-            console.log(data);
-        }else {
-            $scope.messages_error = 'Oops, we received your request, but there was an error processing it.';
-        }
-    })
-    .error(function(data, status, headers, config) {
-        $scope.messages_error = 'There was a network error. Try again later.';
-    });
-}]);
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html',
+            controller: "HomeController"
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('main', {
+            url: '/main',
+            templateUrl: 'app/default/main/main.html'
+            controller: 'mainCtrl'
+        });
+
+});
