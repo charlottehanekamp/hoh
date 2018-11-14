4c95ac321b4d6fdc1fa4682112bacf7a5fedf011
C Hanekamp <charlotte.hanekamp@hva.nl>
2018-11-14 14:25:03 +0100

    removed merge lines

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,7 +1,6 @@
 class AgendaMailer < ApplicationMailer
      default from: 'info@hoh-archery.nl'
      def agenda_email
-<<<<<<< HEAD
           # @users = User.all
           # User.find_each do |user|
                # @email = @user.email
@@ -9,9 +8,5 @@ class AgendaMailer < ApplicationMailer
                mail(:to => @email,
                     :subject => 'Agenda this month')#add an date and maybe some more info
           # end
-=======
-          @user = params[:user]
-          mail(to: @user.email, subject: 'Agenda')
->>>>>>> 40fedb10cf6b27c48f3c338c9290c0165c764e2b
      end
 end
