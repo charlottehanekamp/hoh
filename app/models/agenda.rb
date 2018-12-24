4c91cdbb099d110ae838baa249c50a104ca84afe
C Hanekamp <charlotte.hanekamp@hva.nl>
2018-12-24 11:46:51 +0100

    removed self.

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,5 +1,5 @@
 class Agenda < ActiveRecord::Base
-     def self.monthly_update
+     def monthly_update
          @user = User.all
          @user.each do |u|
            AgendaMailer.monthly_mail(u.email).deliver
